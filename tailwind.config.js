e212011cedd9ef4613aa4fb76e027d2f2dc8726f
Md Tipu Sultan <greenviewsoft@gmail.com>
2022-07-21 11:36:52 +0600

    first commit ecommace by mahabub

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ["./*html"],
+  theme: {
+    screens: {
+      sm : '576px',
+      md : '768px',
+      lg : '992px',
+      xl : '1200px',
+    },
+    container : {
+      center : true,
+      padding : '1rem'  // 16px
+    },
+    extend: {
+      colors : {
+        primary : '#FD3D57',
+      },
+      fontFamily : {
+        'poppins' : ['Poppins'],
+        'roboto' : ['Roboto'],
+      }
+    },
+  },
+  plugins: [],
+}
